2786f9d0411b1475417de55d2c9b3418003f931e
polurumanasa3 <manasap142@gmail.com>
2025-05-31 10:57:52 +0530

    Add files via upload

---
+++ create aform react.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function App() {
+return(
+<>
+<form>
+  Name:<input type='text' placeholder='enter your name'/><br></br>
+  Password<input type='password'  placeholder='enter youtr password'/>
+  Gender:<br> </br>
+  female:<input type='radio' name='a' /><br></br>
+  male::<input type='radio' name='a'/><br></br>
+  Other:<input type='radio' name='a' /><br></br>
+</form>
+</>
+
+  );
+}
+export default App;
+    
